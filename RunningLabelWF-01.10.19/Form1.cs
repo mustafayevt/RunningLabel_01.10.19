04486504069676e7c8974c4430fd8e52350a9c55
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2019-01-11 00:27:04 +0400

    final

--- RunningLabelWF-01.10.19/Form1.cs
+++ RunningLabelWF-01.10.19/Form1.cs
@@ -14,7 +14,21 @@ namespace RunningLabelWF_01._10._19
     {
         public Form1()
         {
+            Label label = new Label();
+            label.Text = "ha ha";
+            label.Size = new Size(35, 15);
+            label.Name = "runningLbl";
+            label.Location = DesktopLocation;
+            label.MouseEnter += Label_MouseEnter;
+            Controls.Add(label);
             InitializeComponent();
+        }
+
+        private void Label_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            (sender as Label).Left = random.Next(0, Width - (sender as Label).Width);
+            (sender as Label).Top = random.Next(0, Height - (sender as Label).Height);
         }
     }
 }
